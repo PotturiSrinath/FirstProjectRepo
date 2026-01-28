c56eec56acd95fa161eebb7ba5baedd95444bc9b
PotturiSrinath <potturisrinath26@gmail.com>
2026-01-28 16:07:36 +0530

    reversenumber.py

--- reversenumber.py
+++ reversenumber.py
@@ -1,9 +1,17 @@
 ''' write a program to find the reverse of the given number'''
 
-num=int(input('enter a number:'))
-rev=0
-while num!=0:
-    r=num%10
-    rev=(rev*10)+r
-    num=num//10
-    print('reverse num is:',rev)
+def reverse(num):
+    rev = 0
+    while num > 0:
+        rev = rev * 10 + num % 10
+        num //= 10
+    return rev  
+
+def ispalindrome(num):
+    return num == reverse(num)
+
+print(reverse(123))        
+print(ispalindrome(123))  
+
+print(reverse(121))        
+print(ispalindrome(121))  
