9dec4994944d46a50289abccb8871cc5ce9196b5
PotturiSrinath <potturisrinath26@gmail.com>
2026-01-29 16:32:31 +0530

    Update factors.py

--- factors.py
+++ factors.py
@@ -1,6 +1,13 @@
 n=int(input("enter a number:"))
 i=1
-while i<n:
+s=0
+while i!=n:
   if n%i==0:
     print(i)
+    s+=i
   i+=1
+  print("Sum of factors is:,s")
+  if s==n:
+    print("perfect number")
+  else:
+    print("not a perfect number")
